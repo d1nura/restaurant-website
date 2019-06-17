da4c85a493d60aaa9a97dc0cbdbce27c070ae99f
d1nura <ddakshitha1@outlook.com>
2019-06-17 14:27:26 +0530

    after server

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 let express = require("express");
 let app = express();
 let fs = require("fs");
+let favicon = require("serve-favicon");
 
 let content = fs.readFileSync("newsData.json");
 let jsonCnt = JSON.parse(content);
 
 app.use(express.static(__dirname + "/assets"));
-
-const PORT = 3000;
+app.use(favicon(__dirname + "/assets/pics/grapes.svg"));
+//const PORT = 3000;
 
 app.set("view engine", "pug");
-<<<<<<< HEAD:index.js
-app.set("port", process.env.PORT);
-=======
-app.set("port",process.env.PORT)
->>>>>>> 6ba9601cb73cbea806a00401f42234ad14d9efdd:index.js
+let port = process.env.PORT || 5500;
 
 app.get("/", (req, res) => {
   res.render("index", { obj: jsonCnt });
@@ -40,10 +37,4 @@ app.get("/news", (req, res) => {
   res.render("news");
 });
 
-<<<<<<< HEAD:index.js
-app.listen(app.get("port"), () => console.log(`listening on ${PORT}`));
-=======
-app.listen(app.get("port"), () =>
-  console.log(`listening on port ${app.get("port")}`)
-);
->>>>>>> 6ba9601cb73cbea806a00401f42234ad14d9efdd:index.js
+app.listen(port, () => console.log(`listening on ${port}`));
